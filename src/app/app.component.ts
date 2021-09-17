d7f08efad0821a20b133f05ee7acb9a7f1404e92
“Patricio <patricio.dilet@gmail.com>
2021-09-17 12:57:22 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+
+  nombre: string = 'Patricio';
+
+  mostrarNombre() {
+    console.log(this.nombre);
+
+  }
 }
